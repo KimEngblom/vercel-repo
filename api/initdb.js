6847b16e87e2049cacb9d1844122df1243edbcf6
engbloki <engbloki@arcada.fi>
2024-01-17 11:07:57 +0200

    initdb added

---
+++ api/initdb.js
@@ -0,0 +1,14 @@
+import { sql } from '@vercel/postgres';
+ 
+export default async function handler(req,res) {
+  try {
+    const result =
+      await sql`CREATE TABLE IF NOT EXISTS pets (id serial, Name varchar);`;
+
+      await sql`ALTER TABLE pets ADD COLUMN IF NOT EXISTS spicies varchar;`;
+
+    return response.status(200).json({ result });
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+}

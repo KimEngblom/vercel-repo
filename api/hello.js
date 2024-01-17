e2facf762ed10169e11f0e7a00dd0cd59809c42c
engbloki <engbloki@arcada.fi>
2024-01-17 11:25:45 +0200

    sda

--- api/hello.js
+++ api/hello.js
@@ -2,6 +2,16 @@ import { sql } from '@vercel/postgres';
 
 export default async function handler(res, req) {
 
+    if (req.method =="post"){
+        const{name,spicies,age} = req.body;
+        await sql`
+        insert into pets(
+            name,spicies,age
+        ) VALUES (
+            ${name},${spicies},${age}
+        )`
+    }
+
     const { rows } = await sql`select * from pets`;
 	res.status(200).json({
         method: req.method,
